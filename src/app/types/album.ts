26c55d5091abd969747a99d259019ca6075c238a
Hélio Souza <contatohelioseverino@gmail.com>
2022-12-22 21:34:08 -0300

    tipos

---
+++ src/app/types/album.ts
@@ -0,0 +1,6 @@
+export interface Album {
+  title: string;
+  descricao: string;
+  data: Date;
+  imagem: string;
+}
---
+++ src/app/types/registro.ts
@@ -0,0 +1,4 @@
+export interface Registro<T> {
+  album: string;
+  dados: T;
+}
